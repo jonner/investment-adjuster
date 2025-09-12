a8bce08efff9e3eb3d6c38cff736ddc1e3834d0c
Jonathon Jongsma <jjongsma@redhat.com>
2025-09-12 08:53:33 -0500

    Add current portfolio summary to the output
    Signed-off-by: Jonathon Jongsma <jjongsma@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -35,17 +35,19 @@ fn main() -> anyhow::Result<()> {
     };
     let account_targets = AccountTarget::load_from_file(targets_path)?;
     println!(
-        "Allocation targets for account {}",
+        "Account {} Target Allocations",
         account_targets.account_number
     );
+    println!(" - Core position");
+    println!(
+        "   - {}: ${} Minimum",
+        account_targets.core_position.symbol, account_targets.core_position.minimum
+    );
+    println!(" - Allocation targets");
     account_targets
         .targets()
         .into_iter()
-        .for_each(|(symbol, percent)| println!(" - {}: {:.1}%", symbol, percent));
-    println!(
-        " - Core position({}): ${} Minimum",
-        account_targets.core_position.symbol, account_targets.core_position.minimum
-    );
+        .for_each(|(symbol, percent)| println!("   - {}: {:.1}%", symbol, percent));
     println!();
 
     let portfolio = Portfolio::load_from_file(opts.current_allocations)?;
@@ -60,18 +62,31 @@ fn main() -> anyhow::Result<()> {
                 account_targets.account_number
             )
         })?;
+
+    println!("Current Allocations");
+    let total: f32 = account.positions.iter().map(|pos| pos.current_value).sum();
+    account.positions.iter().for_each(|pos| {
+        println!(
+            "  - {} ${} ({:.2}%)",
+            pos.symbol,
+            pos.current_value,
+            pos.current_value / total * 100.0
+        )
+    });
+    println!();
+
     let actions = account_targets.process(account)?;
     println!("In order to maintain your target allocations, the following actions are necessary.");
     println!("Sell:");
     actions.iter().for_each(|(symbol, action)| {
         if let Action::Sell(val) = action {
-            println!(" - {symbol}: {val:.2}");
+            println!(" - {symbol}: ${val:.2}");
         }
     });
     println!("Buy:");
     actions.iter().for_each(|(symbol, action)| {
         if let Action::Buy(val) = action {
-            println!(" - {symbol}: {val:.2}")
+            println!(" - {symbol}: ${val:.2}")
         }
     });
     Ok(())
