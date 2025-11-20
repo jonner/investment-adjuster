b8ae09a2b9741c6afb833e9a52727f2364fc5f7e
Jonathon Jongsma <jjongsma@redhat.com>
2025-11-20 11:13:54 -0600

    Improve cli help output
    Signed-off-by: Jonathon Jongsma <jjongsma@redhat.com>

--- src/cli.rs
+++ src/cli.rs
@@ -9,26 +9,35 @@ pub(crate) struct Cli {
     #[arg(
         short,
         long,
-        help = "Override global target allocation configuration file"
+        value_name = "CONFIG_FILE",
+        help = "Override default target allocation configuration file"
     )]
     pub target: Option<PathBuf>,
     #[arg(
-        value_name = "CSV FILE",
-        help = "A CSV file containing account balances"
+        value_name = "ACCOUNT_BALANCES",
+        help = "A file containing account balances"
     )]
     pub account_balance: PathBuf,
     #[arg(
         short,
         long,
         value_delimiter = ',',
+        value_name = "SYMBOL",
         help = "Ignore the specified holdings when calculating target allocations"
     )]
     pub ignore: Vec<String>,
     #[arg(
         long,
-        help = "Amount to keeep in core position (overrides target allocation configuration)"
+        value_name = "VALUE",
+        help = "Amount to keeep in core position (overrides target allocation configuration for all accounts)"
     )]
     pub core_minimum: Option<f32>,
-    #[arg(short, long, value_enum, default_value_t = Provider::Fidelity, help = "Investment provider")]
+    #[arg(
+        short,
+        long,
+        value_enum,
+        value_name = "PROVIDER_ID",
+        default_value_t = Provider::Fidelity,
+        help = "Investment provider associated with account balances file")]
     pub provider: Provider,
 }
