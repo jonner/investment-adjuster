136d497fa339d571e84e37718c5528f50ae36761
Jonathon Jongsma <jonathon@quotidian.org>
2026-03-25 10:37:51 -0500

    Move output-related functions to a separate module
    Signed-off-by: Jonathon Jongsma <jonathon@quotidian.org>

--- src/main.rs
+++ src/main.rs
@@ -5,67 +5,13 @@ use tracing::warn;
 use anyhow::anyhow;
 use clap::Parser;
 use directories::ProjectDirs;
-use tabled::{
-    Table, Tabled,
-    settings::{
-        Alignment, Color, Style,
-        object::{Columns, Rows},
-    },
-};
 
 use crate::cli::AdjustArgs;
 
 mod account;
 mod cli;
+mod output;
 mod provider;
-
-fn display_optional_dollar(val: &Option<Dollar>) -> String {
-    if let Some(val) = val {
-        format!("${val:.2}")
-    } else {
-        "".to_string()
-    }
-}
-
-fn display_dollar(val: &Dollar) -> String {
-    format!("${val:.2}")
-}
-
-fn display_percentage(val: &Percent) -> String {
-    format!("{val:.1}%")
-}
-
-fn display_optional_percentage(val: &Option<Percent>) -> String {
-    if let Some(val) = val {
-        display_percentage(val)
-    } else {
-        "".to_string()
-    }
-}
-
-#[derive(Debug, Tabled)]
-#[tabled(display(Dollar, "display_dollar"))]
-#[tabled(display(Option<Dollar>, "display_optional_dollar"))]
-#[tabled(display(Percent, "display_percentage"))]
-#[tabled(display(Option<Percent>, "display_optional_percentage"))]
-struct AllocationTableRow {
-    #[tabled(rename = "Symbol")]
-    symbol: String,
-    #[tabled(rename = "Value")]
-    current_value: Dollar,
-    #[tabled(rename = "Percent")]
-    current_percentage: Percent,
-    #[tabled(rename = "Target")]
-    target: Option<Percent>,
-    #[tabled(rename = "Sell")]
-    sell: Option<Dollar>,
-    #[tabled(rename = "Buy")]
-    buy: Option<Dollar>,
-    #[tabled(rename = "Result")]
-    result: Option<Dollar>,
-    #[tabled(skip)]
-    ignore: bool,
-}
 
 fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
@@ -156,60 +102,13 @@ fn adjust_command<P: AsRef<Path>>(args: AdjustArgs, config_path: P) -> Result<()
         config.ignored.extend(args.ignore.iter().cloned());
 
         let adjustments = config.adjust_allocations(&account)?;
+        let table = output::format_adjustments(adjustments);
 
         println!(
             "Account {}: {}",
             account.account_number, account.account_name
         );
-        let total: f32 = adjustments
-            .iter()
-            .map(|adj| adj.position.current_value)
-            .sum();
-        let rows: Vec<AllocationTableRow> = adjustments
-            .iter()
-            .map(|adj| AllocationTableRow {
-                symbol: adj.position.symbol.clone(),
-                current_value: adj.position.current_value,
-                current_percentage: adj.position.current_value / total * 100.0,
-                target: Some(adj.target),
-                buy: match adj.action {
-                    Action::Buy(val) => Some(val),
-                    _ => None,
-                },
-                sell: match adj.action {
-                    Action::Sell(val) => Some(val),
-                    _ => None,
-                },
-                result: Some(
-                    adj.position.current_value
-                        + match adj.action {
-                            Action::Sell(val) => -val,
-                            Action::Buy(val) => val,
-                            _ => 0.0,
-                        },
-                ),
-                ignore: matches!(adj.action, Action::Ignore),
-            })
-            .collect();
-        let ignored_rows = find_ignored_rows(&rows);
-        let mut table = Table::new(rows);
-        table.with(Style::rounded());
-        table.modify(Columns::new(..), Alignment::right());
-        for r in ignored_rows {
-            table.modify(Rows::one(r), Color::rgb_fg(150, 150, 150));
-        }
         println!("{table}\n");
     }
     Ok(())
-}
-
-fn find_ignored_rows(rows: &[AllocationTableRow]) -> Vec<usize> {
-    let mut ignored_rows = Vec::new();
-    for (i, row) in rows.iter().enumerate() {
-        if row.ignore {
-            // header row is technically the first row
-            ignored_rows.push(i + 1)
-        }
-    }
-    ignored_rows
 }
